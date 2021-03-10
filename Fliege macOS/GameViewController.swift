5e63a19b9e8b6703b127451739db86d32158000b
Daniel Jilg <winsmith@winsmith.de>
2021-03-10 19:58:38 +0100

    Initial Commit

---
+++ Fliege macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  Fliege macOS
+//
+//  Created by Daniel Jilg on 10.03.21.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
